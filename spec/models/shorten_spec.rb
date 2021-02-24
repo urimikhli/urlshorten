cedea0158f49a466c23ee00b1f8994fa849b7c68
Uri Mikhli <urimikhli@Uris-Mac-mini.local>
2021-02-23 21:37:08 -0500

    Good Breaking spec for catching empty slug

--- spec/models/shorten_spec.rb
+++ spec/models/shorten_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Shorten, type: :model do
     it 'tests that shorten is valid' do
       expect(shorten).to be_valid
     end
+    it ' shorten is invalid if it has an invalid slug' do
+      shorten.slug = ''
+      expect(shorten).not_to be_valid
+      expect(shorten.errors[:slug]).to include("can't be blank")
+    end
+  end
 end
