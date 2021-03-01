63111723aaf8b955b3197b1917225eccec643ff3
Uri Mikhli <urimikhli@Uris-Mac-mini.local>
2021-03-01 13:54:56 -0500

    Breaking Specs for slug uniqueness

--- spec/models/shorten_spec.rb
+++ spec/models/shorten_spec.rb
@@ -2,15 +2,35 @@ require 'rails_helper'
 
 RSpec.describe Shorten, type: :model do
   describe '#validations' do
-    let(:shorten) { build(:shorten) }
+    let(:shorten) { create(:shorten) }
+    let(:shorten_another) { build(:shorten) }
 
-    it 'tests that shorten is valid' do
+    before do
+      shorten
+      shorten_another
+    end
+
+    it 'tests that factory is valid' do
       expect(shorten).to be_valid
     end
-    it ' shorten is invalid if it has an invalid slug' do
+
+    it ' has an invalid slug' do
       shorten.slug = ''
       expect(shorten).not_to be_valid
       expect(shorten.errors[:slug]).to include("can't be blank")
     end
+
+    it ' slugs should be uniq' do
+      #puts("shorten: ", shorten.to_json)
+      #slugs are factory built as identical
+      # TODO: make factory give uniq slugs
+      expect(shorten_another).not_to be_valid
+    end
+
+    it ' slug uniqueness should be case insensative' do
+      shorten_another.slug = "Slug"
+      expect(shorten_another).not_to be_valid
+    end
+
   end
 end
