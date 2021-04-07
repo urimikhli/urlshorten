8da8bcd00f4ae53599507b5dd912baf4342a811c
Uri Mikhli <urimikhli@gmail.com>
2021-04-07 12:51:37 -0400

    WIP Install and set up jsonapi

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # no index route, Eventually will redirect to current User urlShortens list.
-  resources :shortens, only: %i[index show create update destroy], param: :slug
+
+  resources :shortens, only: %i[show ], param: :slug
+  
+  jsonapi_resources :shortens, only: %i[index create update destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
